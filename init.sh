c0649b5c464179ce4f43634149f969402e4a707d
ELENA\ztrixack <ztrixack.th@gmail.com>
2017-06-20 06:53:14 +0700

    add ssl and nginx setup

--- init.sh
+++ init.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 echo "********************"
 cd ~
-apt-get update -y
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get install nginx -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install certbot nginx -y
 
-apt-get purge nodejs=legacy
+sudo cp -f /etc/nginx/sites-available/default /etc/nginx/sites-available/default.0.bak
+
+sudo apt-get purge nodejs=legacy
 wget https://nodejs.org/dist/v6.11.0/node-v6.11.0-linux-x64.tar.gz
 mkdir node
 tar xvf node-v*.tar.?z --strip-components=1 -C ./node
 cd ~
 rm -rf node-v*
 mkdir node/etc
-echo 'prefix=/user/local' > node/etc/npmrc
+echo 'prefix=/usr/local' > node/etc/npmrc
 sudo mv node /opt/
 sudo chown -R root: /opt/node
 sudo ln -s /opt/node/bin/node /usr/local/bin/node
 sudo ln -s /opt/node/bin/npm /usr/local/bin/npm
-apt-get autoremove
+sudo apt-get autoremove
 
 export PATH=$PATH:/usr/local/bin
 
---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "********************"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+npm update --global yarn pm2 serve
+sudo apt-get autoremove
+
+echo "--------------------"
+echo "Done!"
+echo "********************"
