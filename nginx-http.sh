3a61c81b1aa3974e2a209c98e1953dd44651f900
ELENA\ztrixack <ztrixack.th@gmail.com>
2017-07-02 15:22:30 +0700

    set new nginx

--- nginx-http.sh
+++ nginx-http.sh
@@ -2,8 +2,10 @@
 echo "********************"
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/nginx.conf
-cp storage/http/blog.conf /etc/nginx/sites-enabled/default
+cp storage/https/blog.conf /etc/nginx/sites-enabled/default
 cp storage/nginx.conf /etc/nginx/nginx.conf
+sudo nginx -t
+sudo systemctl restart nginx
 echo "--------------------"
 echo "Done!"
 echo "********************"
