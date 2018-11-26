ac987d0514505c57dc8baa30bb06f22845450f13
ztrixack <ztrixack.th@gmail.com>
2018-11-26 19:42:34 +0700

    add setup script from A.praparn

---
+++ general/ntp.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo timedatectl set-timezone Asia/Bangkok
+sudo apt install -y chrony
+sudo sed -i -e 's/pool 2.debian.pool.ntp.org offline iburst/server time.navy.mi.th prefer iburst/g' /etc/chrony/chrony.conf
+sudo /etc/init.d/chrony restart
